7b4ab8bac2f7f0c27d995f3ae0f7e4aca842fd8a
cvand <vandera.chryssanthi@gmail.com>
2014-09-25 23:22:13 -0400

    project creation

---
+++ SwiftLearner/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftLearner
+//
+//  Created by Chrysanthi Vandera on 9/25/14.
+//  Copyright (c) 2014 Worst Team Ever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
